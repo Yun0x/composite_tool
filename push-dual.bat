1c01f77c34c77ce7daae6151f85a300536153df3
YunOx <uu258000@gmail.com>
2026-07-08 10:41:06 +0800

    新增登录页，新增多数据源配置，添加强制跳转

--- push-dual.bat
+++ push-dual.bat
@@ -12,8 +12,8 @@ REM ============================================================
 
 REM ===================== 你只需要改这里 =====================
 
-set "GITEE_URL=https://gitee.com/yunox/auto_like.git"
-set "GITHUB_URL=git@github.com:Yun0x/auto_like.git"
+set "GITEE_URL=git@gitee.com/yunox/composite-tool.git"
+set "GITHUB_URL=git@github.com:Yun0x/common_tool.git"
 
 REM 远程仓库名称，一般不用改
 set "GITEE_REMOTE=gitee"
